f0edf125b650c5ba19532d9bb68c1ea12188221c
Momosundeass <tipok@trueaxiongames.com>
2020-10-30 07:23:18 +0700

    implement frame skip

---
+++ Assets/MainOP2.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+
+public class MainOP2 : MonoBehaviour
+{
+    public int SpheresMax = 10000;
+    public int Speed = 500;
+    private Transform[] spheres;
+    private float[] deltaTimes;
+    private int deltaTimeWriteIndex = 0;
+    private int objPerFrame = 0;
+
+    private readonly static Quaternion identity = Quaternion.identity;
+
+    [SerializeField]
+    private GameObject prefab = default;
+
+    [SerializeField]
+    private int frameSkip = 4;
+
+    void Start()
+    {
+        spheres = new Transform[SpheresMax];
+        deltaTimes = new float[frameSkip];
+        objPerFrame = SpheresMax / frameSkip;
+
+        for (int i = 0; i < SpheresMax; i++)
+        {
+            spheres[i] = Instantiate(prefab, GetSphereInitPosition(), identity).transform;
+        }
+    }
+
+    void Update()
+    {
+        deltaTimes[deltaTimeWriteIndex] = Time.deltaTime;
+
+        float delta = 0;
+        for (int i = 0; i < frameSkip; i++)
+        {
+            delta += deltaTimes[i];
+        }
+        delta = delta * Speed;
+        for (int i = objPerFrame * deltaTimeWriteIndex; i < objPerFrame * (deltaTimeWriteIndex + 1); i++)
+        {
+            var st = spheres[i];
+            Vector3 p = st.position;
+            p.z -= delta;
+            if (p.z < 0)
+            {
+                st.position = GetSphereInitPosition();
+            }
+            else
+            {
+                st.position = p;
+            }
+        }
+
+        deltaTimeWriteIndex++;
+        deltaTimeWriteIndex = deltaTimeWriteIndex >= frameSkip ? 0 : deltaTimeWriteIndex;
+    }
+
+    private Vector3 GetSphereInitPosition()
+    {
+        var position = Random.insideUnitSphere * 500;
+        position.z += 500;
+        return position;
+    }
+}
---
+++ Assets/ShowFPS.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowFPS : MonoBehaviour
+{
+    private float fpsDeltaTime;
+    private bool isInit = false;
+    GUIStyle style;
+    Rect rect;
+
+    void Update()
+    {
+        fpsDeltaTime += (Time.unscaledDeltaTime - fpsDeltaTime) * 0.1f;
+    }
+
+    private void OnGUI()
+    {
+        if (!isInit)
+        {
+            int w = Screen.width, h = Screen.height;
+            style = new GUIStyle();
+            rect = new Rect(0, 0, w, h * 2 / 100);
+            style.alignment = TextAnchor.UpperLeft;
+            style.fontSize = h * 3 / 100;
+            style.normal.textColor = new Color(1.0f, 1.0f, 1.5f, 1.0f);
+            isInit = true;
+        }
+        float msec = fpsDeltaTime * 1000.0f;
+        float fps = 1.0f / fpsDeltaTime;
+        string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+        GUI.Label(rect, text, style);
+    }
+}
